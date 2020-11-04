a82f0cdd095b82f2dce2b76d0886992014861fd7
mmmenezes <53903641+mmmenezes@users.noreply.github.com>
2020-11-04 16:56:21 -0300

    reps,services,models buscated

---
+++ ABCBrasil.OpenBanking.BackOfficeTed.Core/Interfaces/Services/ITedService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ABCBrasil.OpenBanking.BackOfficeTed.Core.Models;
+using ABCBrasil.OpenBanking.BackOfficeTed.Core.Models.Repository;
+
+namespace ABCBrasil.OpenBanking.BackOfficeTed.Core.Interfaces.Services
+{
+    public interface ITedService
+    {
+        void BuscaTeds(BuscaTedRequest tedRequest);
+    }
+}
---
+++ ABCBrasil.OpenBanking.BackOfficeTed.Core/Models/Repository/BuscaTedRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABCBrasil.OpenBanking.BackOfficeTed.Core.Models.Repository
+{
+    public class BuscaTedRequest
+    {
+        public DateTime DTINI { get; set; }
+        public DateTime DTFIM { get; set; }
+        public int QTD { get; set; }
+    }
+}
