df68eed4a4de584a363310cd2f2688acfa6d6f74
mmmenezes <mathmelomenezes@gmail.com>
2020-11-20 09:17:45 -0300

    Metodo na controller Correção de service...

--- ABCBrasil.OpenBanking.BackOfficeTed.Api/Controllers/ReprocessarController.cs
+++ ABCBrasil.OpenBanking.BackOfficeTed.Api/Controllers/ReprocessarController.cs
@@ -9,6 +9,7 @@ using ABCBrasil.OpenBanking.BackOfficeTed.Core.Services;
 using ABCBrasil.SegurancaApi.DSL.Libs.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace ABCBrasil.OpenBanking.BackOfficeTed.Api.Controllers
@@ -33,12 +34,20 @@ namespace ABCBrasil.OpenBanking.BackOfficeTed.Api.Controllers
 
         //[ProducesResponseType(typeof(ApiResult<ComprovanteResponse>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResult), StatusCodes.Status400BadRequest)]
-        [HttpGet( Name = "PopulaTabela")]
-        public async Task<IActionResult> PopulaTabela(BuscaTedRequest tedRequest)
+        [HttpGet( Name = "BuscaTeds/{dtini}/{dtfim}/{qtd}")]
+        public async Task<IActionResult> PopulaTabela(DateTime dtini,DateTime dtfim,int qtd)
         {
-            var teds = _tedService.BuscaTeds(tedRequest);
+            var teds = _tedService.BuscaTeds(new BuscaTedRequest { DTINI = dtini, DTFIM = dtfim, QTD = qtd });
 
             return Response<string>(teds, System.Net.HttpStatusCode.OK);
+        }
+        [ProducesResponseType(typeof(ApiResult), StatusCodes.Status400BadRequest)]
+        [HttpPost(Name = "ReprocessaTed")]
+        public async Task<IActionResult> ReprocessaTed(string SelectedCSV)
+        {
+
+            var res=  _tedService.ProcessaTed(SelectedCSV);
+            return Response<bool>(res, System.Net.HttpStatusCode.OK);
         }
     }
 }
